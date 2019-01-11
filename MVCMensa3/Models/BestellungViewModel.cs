d8498b424b800502873db477c5f610e021a0b2e5
Frederik Engels <frederik.engels24@gmail.com>
2019-01-11 02:58:07 -0800

    some progress, idk my commits are bad

---
+++ MVCMensa3/Models/BestellungViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCMensa3.Models
+{
+    public class BestellungViewModel
+    {
+        public class BestellungView
+        {
+            public int ID { get; set; }
+            public String Name { get; set; }
+            public decimal Preis { get; set; }
+            [Range(0, 10)]
+            public int Anzahl { get; set; }
+
+            public BestellungView(int id, String name, decimal preis, int anz)
+            {
+                ID = id;
+                Name = name;
+                Preis = preis;
+                Anzahl = anz;
+            }
+        }
+
+        public IEnumerable<BestellungView> Bestellungen { get; set; }
+        public DateTime AbholZeit { get; set; }
+
+        public BestellungViewModel(IEnumerable<BestellungView> bestellungen, DateTime abholZeit)
+        {
+            Bestellungen = bestellungen;
+            AbholZeit = abholZeit;
+        }
+    }
+}
---
+++ MVCMensa3/Models/Head.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCMensa3.Models
+{
+    public class Head
+    {
+        public int? AnzahlBestellungen { get; set; }
+
+        public Head()
+        {
+            AnzahlBestellungen = null;
+        }
+
+        public Head(int? anzBestellungen)
+        {
+            AnzahlBestellungen = anzBestellungen;
+        }
+    }
+}
